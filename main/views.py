8011db4fa7f197ab5404043bf0e851d1d011927f
Annewangari <annekare3@gmail.com>
2020-10-20 22:56:39 +0300

    set all routes to the other html documents

--- main/views.py
+++ main/views.py
@@ -1,8 +1,16 @@
 from flask import render_template
 from app import app
-
-
 @app.route('/')
 def index():
 
     return render_template('index.html')
+
+@app.route('/news-article')
+def index():
+
+    return render_template('news-article.html')
+
+@app.route('/news-source')
+def index():
+
+    return render_template('news-source.html')
