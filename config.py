26f5215ea9e04f713fc517a3ce173684c4e39599
Annewangari <annekare3@gmail.com>
2020-10-20 23:26:37 +0300

    API url set in config file

--- config.py
+++ config.py
@@ -1,14 +1,10 @@
 class Config:
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/{}country=us&apiKey={}'
+
 
     pass
-
-
 
 class ProdConfig(Config):
-
     pass
-
-
 class DevConfig(Config):
-
     DEBUG = True
