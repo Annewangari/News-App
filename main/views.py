7fe776dc5730235d79944d4727a265ee35d04e7d
Annewangari <annekare3@gmail.com>
2020-10-21 13:18:48 +0300

    update views together with relevant html files

--- main/views.py
+++ main/views.py
@@ -7,10 +7,16 @@ def index():
 
 @app.route('/news-article')
 def index():
+@app.route('/newsArticle')
+def newsArticle():
 
     return render_template('news-article.html')
+    return render_template('newsArticle.html')
 
 @app.route('/news-source')
 def index():
+@app.route('/newsSource')
+def newsSource():
 
     return render_template('news-source.html')
+    return render_template('newsSource.html')
