3904053e4bc71614d3e81f1ce0afe8fddbac5285
Annewangari <annekare3@gmail.com>
2020-10-22 00:11:13 +0300

    deployement to heroku

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export NEWS_API_KEY=525ae38a499a41b78c74314ee571668b
-export SECRET_KEY=wati
+export NEWS_API_KEY='525ae38a499a41b78c74314ee571668b'
+export SECRET_KEY='Karehmwangi'
 
 python3.6 manage.py server
