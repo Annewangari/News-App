13f33e3829218d39b89e8de140e054e8b7f07339
Annewangari <annekare3@gmail.com>
2020-10-21 13:22:10 +0300

    set tittle

--- main/views.py
+++ main/views.py
@@ -5,18 +5,12 @@ def index():
 
     return render_template('index.html')
 
-@app.route('/news-article')
-def index():
+    title = 'Home - Welcome to your number one news site'
+    return render_template('index.html', title = title)
+
 @app.route('/newsArticle')
 def newsArticle():
-
-    return render_template('news-article.html')
     return render_template('newsArticle.html')
-
-@app.route('/news-source')
-def index():
 @app.route('/newsSource')
 def newsSource():
-
-    return render_template('news-source.html')
     return render_template('newsSource.html')
